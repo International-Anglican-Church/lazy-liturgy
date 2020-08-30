a50cd12bcebfe3a1d3a21a3e111da2b51293e8ca
Andrew Blomenberg <andrewBlomen@gmail.com>
2020-08-30 15:41:17 -0600

    Underlining, bolding, and bullets are carried through

--- main.py
+++ main.py
@@ -49,36 +49,51 @@ def get_document(doc_alias, docs_service):
     return docs_service.documents().get(documentId=doc_id).execute()
 
 
-def get_tag_value(source_doc, tag):
+def get_tag_paragraphs(source_doc, tag):
     collecting_text = False
-    tag_pattern = r'{{(.+)}}'
-    out_text = ''
+    tag_pattern = r'{{([^{}]+)}}'
+    out = []
 
     paragraphs = filter(lambda elem: 'paragraph' in elem, source_doc['body']['content'])
     for para in paragraphs:
-        para_content = para['paragraph']['elements'][0]['textRun']['content']
 
-        if re.match(tag_pattern, para_content):
-            if re.match(tag_pattern, para_content).group(1) == tag:
-                collecting_text = True
+        for para_elem in para['paragraph']['elements']:
+            if re.search(tag_pattern, para_elem['textRun']['content']):
+                if re.findall(tag_pattern, para_elem['textRun']['content'])[0] == tag:
+                    collecting_text = True
+                else:
+                    collecting_text = False
+
             elif collecting_text:
-                return out_text.strip()
+                out.append(para_elem['textRun'])
 
-        elif collecting_text:
-            out_text += para_content
+    if not len(out):
+        raise ValueError(f'tag "{tag}" not found')
 
-    return out_text.strip()
+    while out[-1]['content'].rstrip() == '':
+        out = out[:-1]
+
+    out[-1]['content'] = out[-1]['content'].rstrip()
+    return out
 
 
-def get_all_tags(source_doc):
-    tags = []
-    tag_pattern = r'{{(.+)}}'
-    paragraphs = filter(lambda elem: 'paragraph' in elem, source_doc['body']['content'])
-    for para in paragraphs:
-        para_content = para['paragraph']['elements'][0]['textRun']['content']
-        if re.match(tag_pattern, para_content):
-            tags.append(re.match(tag_pattern, para_content).group(1))
+def get_document_content(doc):
+    out = ''
+    for doc_elem in doc['body']['content']:
+        if 'paragraph' in doc_elem:
+            for para_elem in doc_elem['paragraph']['elements']:
+                if 'textRun' in para_elem:
+                    out += para_elem['textRun']['content']
+                elif 'inlineObjectElement' in para_elem:
+                    out += '📷'
+    return out
 
+
+def get_all_tags(doc):
+    content = get_document_content(doc)
+    tag_pattern = r'{{([^{}]+)}}'
+    tags = re.findall(tag_pattern, content)
+    tags.reverse()
     return tags
 
 
@@ -86,7 +101,8 @@ def copy_template(doc_alias, drive_service, docs_service):
     source_doc = get_document('source', docs_service)
     template_doc = get_document(doc_alias, docs_service)
 
-    string_date = get_tag_value(source_doc, 'date')
+    date_content = [para['content'] for para in get_tag_paragraphs(source_doc, 'date')]
+    string_date = "".join(date_content)
     date = datetime.datetime.strptime(string_date, '%B %d, %Y')
     string_date = date.strftime('%Y-%m-%d')
     doc_title = f'{string_date} {DOCUMENT_TITLES[doc_alias]}'
@@ -96,12 +112,14 @@ def copy_template(doc_alias, drive_service, docs_service):
         body={'name': doc_title}
     ).execute()
 
-    return drive_service.files().update(
+    new_file = drive_service.files().update(
         fileId=new_file['id'],
         addParents=FOLDERS['dest'],
         removeParents=FOLDERS['source'],
         fields='id, parents'
     ).execute()
+
+    return docs_service.documents().get(documentId=new_file['id']).execute()
 
 
 def main():
@@ -111,18 +129,57 @@ def main():
     source_doc = get_document('source', docs_service)
     for doc_alias in ['booklet', 'portrait']:
         new_doc = copy_template(doc_alias, drive_service, docs_service)
+        new_doc_contents = get_document_content(new_doc)
 
-        requests = [
-            {
+        requests = []
+        for tag in get_all_tags(new_doc):
+            paragraphs = get_tag_paragraphs(source_doc, tag)
+
+            replace_text = "".join([para['content'] for para in paragraphs])
+
+            requests.append({
                 'replaceAllText': {
                     'containsText': {'text': '{{' + tag + '}}'},
-                    'replaceText': get_tag_value(source_doc, tag)
+                    'replaceText': replace_text
                 }
-            }
-            for tag in get_all_tags(source_doc)
-        ]
+            })
 
-        docs_service.documents().batchUpdate(documentId=new_doc['id'], body={'requests': requests}).execute()
+            indices = [new_doc_contents.index('{{' + tag + '}}') + 1]
+            for para in paragraphs:
+                indices.append(indices[-1] + len(para['content']))
+                for style in ['bold', 'underline']:
+                    if style in para['textStyle']:
+                        requests.append({
+                            'updateTextStyle': {
+                                'textStyle': {style: True},
+                                'range': {'startIndex': indices[-2], 'endIndex': indices[-1]},
+                                'fields': style
+                            }
+                        })
+
+            if tag in ('announcements', 'lunch questions'):
+                requests.append({
+                    'createParagraphBullets': {
+                        'range': {'startIndex': indices[0], 'endIndex': indices[-1]},
+                        'bulletPreset': 'BULLET_DISC_CIRCLE_SQUARE'
+                    }
+                })
+
+            if tag == 'announcements':
+                indices = indices[1::2]  # remove half of the indices
+                indices.reverse()
+                for index in indices:
+                    requests += [
+                        {'deleteParagraphBullets': {'range': {
+                            'startIndex': index,
+                            'endIndex': index + 1
+                        }}},
+                        {'insertText': {'text': '\t', 'location': {
+                            'index': index
+                        }}}
+                    ]
+
+        docs_service.documents().batchUpdate(documentId=new_doc['documentId'], body={'requests': requests}).execute()
 
 
 if __name__ == '__main__':
