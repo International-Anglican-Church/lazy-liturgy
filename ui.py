b2e286bf157aa696dfcc44db547efd413dddef2d
Andrew Blomenberg <andrewBlomen@gmail.com>
2020-10-02 21:35:16 -0700

    Added actually useful status message

--- ui.py
+++ ui.py
@@ -9,13 +9,18 @@ import wordpress
 
 
 def main():
+    print('Thanks for being lazy!')
+    print('Logging in')
     drive_service = build('drive', 'v3', credentials=google_docs.get_creds())
     docs_service = build('docs', 'v1', credentials=google_docs.get_creds())
-
+    
+    print('Getting this week\'s info')
     source_doc = google_docs.get_document('source', docs_service)
+
     for doc_alias in ['booklet', 'portrait']:
         requests = []
-
+        
+        print(f'Creating new {doc_alias} doc')
         new_doc = google_docs.copy_template(doc_alias, drive_service, docs_service)
         template_text = google_docs.get_document_content(new_doc)
 
@@ -24,18 +29,20 @@ def main():
         for tag in tags:
             tag_text_runs = google_docs.get_tag_text_runs(source_doc, tag)
             requests += google_docs.get_tag_insertion_requests(tag, tag_text_runs, template_text)
-
+        
+        print(f'Writing to {doc_alias} doc')
         docs_service.documents().batchUpdate(documentId=new_doc['documentId'], body={'requests': requests}).execute()
-
+    
+    print('Getting Wordpress template')
     template_html = wordpress.get_page_html(wordpress.TEMPLATE_ID)
     for tag in wordpress.get_all_tags(template_html):
         tag_text_runs = google_docs.get_tag_text_runs(source_doc, tag)
         template_html = wordpress.insert_tag_with_formatting(tag_text_runs, tag, template_html)
-
+    
+    print('Writing to Wordpress page')
     wordpress.set_page_html(wordpress.GENERATED_ID, template_html)
 
 
 if __name__ == '__main__':
-    print('TIME FOR SOUP')
     main()
-    print('ALL SLURPED UP')
+    print('Done!')
